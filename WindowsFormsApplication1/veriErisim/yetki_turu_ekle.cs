9af1e827534751cfddafcfb37eb94a812484dc0e
esame <esame@DESKTOP-ARSNVCC>
2023-06-20 00:44:24 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApplication1/veriErisim/yetki_turu_ekle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.veriErisim
+{
+    class yetki_turu_ekle
+    {
+        public static void ekle(varliklar.yetki_turu eklenecek)
+        {
+            veriTabani.baglantiKoptuysaAc();
+            SqlCommand Comm = new SqlCommand("yetkiTuruEkle", veriTabani.baglanti); 
+            Comm.CommandType = CommandType.StoredProcedure;
+            SqlParameter p1 = new SqlParameter("@yetki_ismi", SqlDbType.VarChar, 45);  
+            p1.Value = eklenecek._yetki_ismi;
+            Comm.Parameters.Add(p1);
+            Comm.ExecuteNonQuery();
+        }
+    }
+}
